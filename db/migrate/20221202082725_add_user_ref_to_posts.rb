a3dd9f74ffea4226e5ea5b4aa0e089d9390bb092
Moathal <moathal.s.kachi@gmail.com>
2022-12-02 13:20:48 +0300

    Fix Linters errors

--- db/migrate/20221202082725_add_user_ref_to_posts.rb
+++ db/migrate/20221202082725_add_user_ref_to_posts.rb
@@ -1,5 +1,6 @@
 class AddUserRefToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :user, null: false, foreign_key: true
+    add_reference :posts,
+                  :user, null: false, foreign_key: true
   end
 end
