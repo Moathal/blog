53b4c6bb90af7c8673b89c3f52128986ae76eb03
Moathal <moathal.s.kachi@gmail.com>
2022-12-09 09:24:38 +0300

    Add controllers for users & posts
    Add nested routs for posts & controllers

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def show
+        @users = User.find(params[:id])
+    end
+
+    def new
+        @users = User.new
+    end
+end
