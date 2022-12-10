989005a92eda4794199c9adf80eddf5aa2577953
Moathal <moathal.s.kachi@gmail.com>
2022-12-10 10:24:27 +0300

    Fix linters

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
+RSpec.describe 'Posts', type: :request do
   subject { Post.new(title: 'My First Post', comments_counter: 4, likes_counter: 20) }
 
   before { subject.save }
 
-  describe "GET /index" do
+  describe 'GET /index' do
     it 'http request returnes success and check correct placeholder text.' do
       get '/users/:author_id/posts'
       expect(response).to have_http_status(:ok)
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe 'Users', type: :request do
   subject { User.new(name: 'John', post_counter: 30, photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.') }
 
   before { subject.save }
-  describe "GET /index" do
-    it "http request returns success and check correct placeholder text." do
-      get "/"
+  describe 'GET /index' do
+    it 'http request returns success and check correct placeholder text.' do
+      get '/'
       expect(response).to have_http_status(:ok)
-      expect(response.body).to include("users")
+      expect(response.body).to include('users')
     end
   end
 
-  describe "GET /show" do
-    it "http request returns success and check correct placeholder text." do
+  describe 'GET /show' do
+    it 'http request returns success and check correct placeholder text.' do
       get "/users/#{subject.id}"
       expect(response).to have_http_status(:ok)
-      expect(response.body).to include("user")
+      expect(response.body).to include('user')
     end
   end
 end
