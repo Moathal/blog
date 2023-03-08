d113065bf0b63280b10abc2eab255ee17be60aa1
Moathal <moathal.s.kachi@gmail.com>
2023-03-08 19:26:56 +0300

    Fix linters error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :except =>[:index, :show]
+  before_action :authenticate_user!, except: %i[index show]
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
