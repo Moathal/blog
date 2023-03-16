d776699682384eb5f09dd1f9d53fb71d705488e4
Moathal <moathal.s.kachi@gmail.com>
2023-03-16 19:41:45 +0300

    Fix the error in Register Controller

--- app/controllers/concerns/bloggers/registrations_controller.rb
+++ app/controllers/concerns/bloggers/registrations_controller.rb
@@ -50,12 +50,12 @@ class Bloggers::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   end
 
-  The path used after sign up.
+  # The path used after sign up.
   def after_sign_up_path_for(resource)
     super(resource)
   end
 
-  The path used after sign up for inactive accounts.
+  # The path used after sign up for inactive accounts.
   def after_inactive_sign_up_path_for(resource)
     super(resource)
   end
