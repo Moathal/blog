b94715f481a2ecbcbcf48fd1e636643b845f0150
Moathal <moathal.s.kachi@gmail.com>
2023-04-16 16:23:17 +0300

    Change rails to rake in app/bin/render-build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,9 +3,9 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
 
 postgres://databasefree_user:YNPnOk0tB2RM1nJodjh3IwUasfHdCWTd@dpg-cf2hk7en6mpkr6eekpog-a/databasefree  DB-URL
 c89d53892dcd07df4f0dd57967162560  RAILS_MASTER_KEY
