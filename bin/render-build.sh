a4df3e0fb0a725343a028740ef54eeb9a9c59b84
Moathal <moathal.s.kachi@gmail.com>
2023-04-16 16:34:40 +0300

    Remove extra stuff in render-build'

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,3 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
-
-postgres://databasefree_user:YNPnOk0tB2RM1nJodjh3IwUasfHdCWTd@dpg-cf2hk7en6mpkr6eekpog-a/databasefree  DB-URL
-c89d53892dcd07df4f0dd57967162560  RAILS_MASTER_KEY
